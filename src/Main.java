772fb2eae6cd739e0cef7d9e66fe93861b996dc2
sanmato <santiago.mato.rpc@gmail.com>
2023-03-16 00:20:02 -0300

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main{
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-
-        Person myPerson = new Person(12);
+        Person myPerson = new Person(13);
         System.out.println(myPerson.getAge());
     }
 }
