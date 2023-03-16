515565e20badc598fcb09fe7ae503dd443afb06e
sanmato <santiago.mato.rpc@gmail.com>
2023-03-15 21:18:32 -0300

    First project Java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+
+        Person myPerson = new Person(12);
+        System.out.println(myPerson.getAge());
+    }
+}
