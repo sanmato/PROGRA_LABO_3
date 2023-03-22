009857ebaa5777dd0473a9f7fc4870a218b85802
sanmato <santiago.mato.rpc@gmail.com>
2023-03-21 23:39:23 -0300

    Guia 1 hecha (falta punto 5)

---
+++ Guia 1/src/BankAccount.java
@@ -0,0 +1,68 @@
+
+public class BankAccount {
+    private int id;
+    private String name;
+    private double balance;
+
+
+    public BankAccount(int id, String name, double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    
+
+    public double depositTransaction(double moneyToDeposit) {
+        double newBalance = balance + moneyToDeposit;
+        setBalance(newBalance);
+        return newBalance;
+    }
+
+    public double extractTransaction(double moneyToExtract) {
+        double newBalance;
+        if(moneyToExtract > balance) {
+            System.out.println("Not enough balance in account to extract $" + moneyToExtract +"!");
+            return balance;
+        } else {
+            newBalance = balance - moneyToExtract;
+            setBalance(newBalance);
+            return newBalance;
+        }
+    }
+
+    public void showAccountDetails(int id, String name, double balance) {
+        System.out.println("CuentaBancaria[id=" + id + ", nombre=" + name + ", saldo=" + balance + "]");
+    }
+
+}
